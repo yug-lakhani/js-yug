f4e131cc0c0dd295c9dc6058060c559b9db9d727
yug-lakhani <yuglakhani01@gmail.com>
2024-06-26 11:44:32 +0000

    datatype and var

---
+++ basic_js/datatype.js
@@ -0,0 +1,13 @@
+"use strict";  //treat all new version
+
+// alert(3+3) we are using nodejs,not browser that's why give error
+
+
+let inte = 7;
+let sting = "sd";
+let bolli = true;
+
+
+console.log(typeof inte);
+
+//NULL -> is an object;
---
+++ basic_js/variable.js
@@ -0,0 +1,11 @@
+ const accountId = 144553
+ let accountEmail = "yuglakhani01@gmail.com"
+ var accountPassword = "124421"
+ num = 5;
+ accountCity = "surat"
+
+ //we not change const
+ // not use var because of scope problem... 
+
+ console.log(num);
+ console.table([accountId,accountEmail,accountPassword,accountCity]);
